69ebdb0a5919d2e057d0a865d120bb59c8c64273
Paulo Ferreira <alunosnet@gmail.com>
2020-11-13 17:04:32 +0000

    started
    First commity

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerJump : MonoBehaviour
+{
+    CharacterController _characterController;
+    [SerializeField] private AnimationCurve jumpFallOff;
+    [SerializeField] private float jumpMultiplier;
+    [SerializeField] private bool isJumping;
+    [SerializeField] bool playerGrounded;
+    Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && isJumping == false)
+        {
+            isJumping = true;
+            if(_animator!=null)
+                _animator.SetTrigger("jump");
+            StartCoroutine(JumpEvent());
+        }
+        playerGrounded = _characterController.isGrounded;
+    }
+    private IEnumerator JumpEvent()
+    {
+        float timeInAir = 0.0f;
+        do
+        {
+            float jumpForce = jumpFallOff.Evaluate(timeInAir) * jumpMultiplier * Time.deltaTime;
+
+            _characterController.Move(Vector3.up * jumpForce);
+
+            timeInAir += Time.deltaTime;
+
+            playerGrounded = _characterController.isGrounded;
+            isJumping = !_characterController.isGrounded;
+
+            yield return null;
+        } while (_characterController.collisionFlags != CollisionFlags.Above && isJumping == true);
+        isJumping = false;
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    float velocidadeAndar = 5;
+    [SerializeField]
+    float velocidadeRodar = 5;
+
+    CharacterController _characterController;
+    Animator _animator;
+    float inputAndar;
+    float inputRodar;
+    bool inputSprint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputAndar = CrossPlatformInputManager.GetAxis("Vertical");
+        inputRodar = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        Vector3 novaPosicao = transform.forward * velocidadeAndar * inputAndar;
+        novaPosicao.y = Physics.gravity.y;
+
+        _characterController.Move(novaPosicao * Time.deltaTime);
+
+        _characterController.transform.Rotate(_characterController.transform.up * velocidadeRodar * inputRodar);
+
+
+        if(_animator!=null) _animator.SetFloat("velocidade", inputAndar);
+    }
+}
