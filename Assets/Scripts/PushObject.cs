5d207ff4f81a3414abf7161b306d8740f057eb06
Paulo Ferreira <alunosnet@gmail.com>
2020-11-15 16:31:59 +0000

    Script para empurrar objetos

---
+++ Assets/Scripts/PushObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushObject : MonoBehaviour
+{
+    [SerializeField] float forca;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.isStatic) return;
+        var rb = other.GetComponent<Rigidbody>();
+        if(rb != null)
+        {
+            Debug.Log("Collidi com " + other.transform.name);
+            Vector3 direcao = other.transform.position - transform.position;
+            rb.AddForce(direcao.normalized * forca);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
