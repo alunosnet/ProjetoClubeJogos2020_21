05ddc814af830a7e45825775d3fc61ed74d1a30c
Paulo Ferreira <alunosnet@gmail.com>
2020-11-15 21:42:51 +0000

    Árvores para mudar de nível

---
+++ Assets/Scripts/Passagem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Este script controla o acesso aos níveis anterior e próximo 
+/// </summary>
+public class Passagem : MonoBehaviour
+{
+    [SerializeField] bool proximo=true;
+    [SerializeField] bool ativo = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (ativo == false) return;
+        if (other.tag.Equals("Player"))
+            if (proximo)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            else
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
