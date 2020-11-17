4d905b3a5c29a3dd90892c5c85c97d9418d94f45
Paulo Ferreira <alunosnet@gmail.com>
2020-11-17 21:44:29 +0000

    Interagir com objetos. Abrir porta.

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E)){
+            var outro = other.GetComponent<IInteract>();
+            if(outro!=null)
+                outro.Action();
+        }
+    }
+}
---
+++ Assets/Scripts/Rodar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rodar : MonoBehaviour,IInteract
+{
+    [SerializeField]
+    bool OneTime = false;
+    [SerializeField]
+    bool Used = false;
+    [SerializeField]
+    float angulo = 45;
+    [SerializeField]
+    float tempo = 3;
+    bool ARodar = false;
+    public void Action()
+    {
+        if (OneTime && Used) return;
+        if (ARodar) return;
+        Used = true;
+        StartCoroutine("Roda");
+    }
+    IEnumerator Roda()
+    {
+        float inc = angulo / tempo;
+        float atual = 0;
+        ARodar = true;
+        while(atual<angulo)
+        {
+            transform.Rotate(0, inc*Time.deltaTime, 0);
+            atual += inc*Time.deltaTime;
+            yield return null;
+        }
+        ARodar = false;
+    }
+}
