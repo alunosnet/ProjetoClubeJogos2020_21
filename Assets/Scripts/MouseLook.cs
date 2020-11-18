960141e57adab0081e888ab6783256288b88fcc9
Paulo Ferreira <alunosnet@gmail.com>
2020-11-18 22:04:35 +0000

    Mouse look para rodar o player

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -20,15 +20,15 @@ public class MouseLook : MonoBehaviour
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+       /// float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90, 90f);
+       // xRotation -= mouseY;
+       // xRotation = Mathf.Clamp(xRotation, -90, 90f);
 
         
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+//transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
         
        
