9871d0461e1fa3291f5303a19f14e3d981e30a8f
Paulo Ferreira <alunosnet@gmail.com>
2020-11-21 09:33:26 +0000

    Nora a rodar

---
+++ Assets/Scripts/NoraRodar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoraRodar : MonoBehaviour
+{
+    public float velocidadeRodar = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, velocidadeRodar*Time.deltaTime, 0);
+    }
+}
